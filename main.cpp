0628bfdc30ace81de2264ece05208742398d65da
shivambhanu <shivambhanu6924@gmail.com>
2025-01-10 23:18:57 +0530

    create blank screen & simulation loop

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
 #include "raylib.h"
 
 int main() {
-    // Initialize the window
-    InitWindow(600, 600, "Animation");
+    const int WINDOW_HEIGHT = 750;
+    const int WINDOW_WIDTH = 750;
+    int fps = 12;
 
-    SetTargetFPS(60);
-    // Vector2 position = {300, 300};
-    int speedOne = 3, speedTwo = 3;
-    int x1 = 20, y1 = 300;
-    int x2 = 300, y2 = 20;
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GAME OF LIFE");
+    SetTargetFPS(fps);
 
-    // Game loop
     while (!WindowShouldClose()) {
-        x1 += speedOne;
-        y2 += speedTwo;
+        // Event Handling
 
-        if (x1 > 580 || x1 < 20) speedOne *= -1;
-        if (y2 > 580 || y2 < 20) speedTwo *= -1;
+        // Updating States
 
-
+        // Drawing
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        DrawCircle(x1, y1, 20, RED);
-        DrawCircle(x2, y2, 20, GREEN);
-
-        //Moving the circle using keys
-        // if (IsKeyDown(KEY_LEFT)) position.x-=5;
-        // if (IsKeyDown(KEY_UP)) position.y-=5;
-        // if (IsKeyDown(KEY_RIGHT)) position.x+=5;
-        // if (IsKeyDown(KEY_DOWN)) position.y+=5;
 
         EndDrawing();
     }
